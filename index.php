ca6ade7be729b073a7e13a51ddd8f2cfa71dd3d9
Cauan Cascaes Senem <ecocauan@gmail.com>
2022-08-29 17:11:40 -0300

    P002

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+include_once("conexao.php");
+print_r($_POST);
+if (isset($_POST['gravar']))
+{
+    $sql = "INSERT INTO viagem(Modelo,Placa,Nome,LocaldeOrigem,LocaldeDestino,KM,LitrosGastos,ValorGasolina) values (:Modelo,:Placa,:Nome,:LocaldeOrigem,:LocaldeDestino,:KM,:LitrosGastos,:ValorGasolina)";
+    $consulta = $conn->prepare($sql);
+    $resultado = $consulta->execute(array("Modelo"=>$_POST['modelo'],"Placa"=>$_POST['placa'],
+                                          "Nome"=>$_POST['nome'],"LocaldeOrigem"=>$_POST['origem'],
+                                          "LitrosGastos"=>$_POST['ltr'],"KM"=>$_POST['percorrido'],"LocaldeDestino"=>$_POST['destino'],
+                                          "ValorGasolina"=>$_POST['valor']));
+    $_POST = null;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +21,26 @@
     <title>Menu</title>
 </head>
 <body>
-    <form action="" method="post">
-        Carro<input type="text">
+    <form action="" method="post" style="text-align:center;">
+        <h1>Cadastro</h1>
+        <hr>
+        Carro/Modelo: <input type="text" id="modelo" name="modelo" required>
+        <hr>
+        Carro/Placa: <input type="text" id="placa" name="placa" required>
+        <hr>
+        Nome: <input type="text" id="nome" name="nome" required>
+        <hr>
+        Local de Origem: <input type="text" id="origem" name="origem" required>
+        <hr>
+        Local de Destino: <input type="text" id="destino" name="destino" required>
+        <hr>
+        KM/Percorridos: <input type="text" id="percorrido" name="percorrido" required>
+        <hr>
+        LTR/Usados: <input type="text" id="ltr" name="ltr" required>
+        <hr>
+        Valor Gasolina: <input type="text" id="valor" name="valor" required>
+        <hr>
+        <input type="submit" name="gravar" id="gravar" value="Gravar">
     </form>
 </body>
 </html>
